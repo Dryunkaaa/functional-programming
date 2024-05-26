4e776c8c0706bd8b2d41af7baa3708216451a079
Andrey Bibikov <andrii.bibikov@iternal.group>
2024-05-26 14:17:02 +0300

    Implemented own functional interface.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        MyFunction<String, Integer> parseIntFunction = Integer::parseInt;
+        MyFunction<String, Integer> squareStringInt = parseIntFunction.andThen(x -> x * x);
 
+        System.out.println(squareStringInt.apply("4")); // should print 16
+    }
+
+    @FunctionalInterface
+    interface MyFunction<T, R> {
+        R apply(T element);
+
+        default <U> MyFunction<T, U> andThen(MyFunction<R, U> afterFunction) {
+            return x -> afterFunction.apply(apply(x));
+        }
     }
 }
