cb286bab540045a9f073db195216d6618786a1e9
Andrey Bibikov <andrii.bibikov@iternal.group>
2024-05-26 19:11:04 +0300

    Added custom collector for join of string.

--- src/main/java/org/example/CustomStreamCollectorApp.java
+++ src/main/java/org/example/CustomStreamCollectorApp.java
@@ -4,18 +4,20 @@ import org.example.bean.User;
 import org.example.utils.Users;
 
 import java.util.Stack;
+import java.util.StringJoiner;
 import java.util.stream.Collector;
 
 public class CustomStreamCollectorApp {
     public static void main(String[] args) {
-        Stack<String> emailsStack = Users.generateUsers(10)
+        Stack<String> emailsStack = Users.generateUsers(15)
                 .stream()
                 .map(User::email)
                 .collect(toStack());
 
-        while (!emailsStack.empty()) {
-            System.out.println(emailsStack.pop());
-        }
+        String joinedEmails = emailsStack.stream()
+                .collect(toJoinString(" | "));
+
+        System.out.println(joinedEmails);
     }
 
     private static <T> Collector<T, ?, Stack<T>> toStack() {
@@ -23,6 +25,15 @@ public class CustomStreamCollectorApp {
             stack2.forEach(stack1::push);
             return stack1;
         });
+    }
+
+    private static Collector<String, StringJoiner, String> toJoinString(CharSequence delimiter) {
+        return Collector.of(
+                () -> new StringJoiner(delimiter),
+                StringJoiner::add,
+                (x, y) -> x.add(y.toString()),
+                StringJoiner::toString
+        );
     }
 
 }
